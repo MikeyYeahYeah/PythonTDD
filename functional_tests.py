8d69a20c581f81bc02e95fb431b505baf3030590
Michael Rosabal <mrosabal@circleci.com>
2016-06-14 18:40:46 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -8,6 +9,7 @@ class NewVisitorTest(unittest.TestCase):
         firefox_capabilities = DesiredCapabilities.FIREFOX
         firefox_capabilities['marionette'] = True
         self.browser = webdriver.Firefox(capabilities=firefox_capabilities)
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -19,14 +21,24 @@ class NewVisitorTest(unittest.TestCase):
 
 # The site title reaffirms that this is in fact a To-Do list site
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 # The user is invited to enter a To-Do list entry right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter To-Do Item')
 
 # The user Types "Buy Milk and Bread"
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy Milk and Bread' for row in rows))
 
 # The user hits enter, the page updates, and presents "1: Buy Milk and Bread"
 
 # The user sees a text box asking them to add another item.  User enters "Buy Chicken Breast"
+
+# Just being thorough
+        self.fail('Finish the test!')
 
 # The page updates again and presents both items
 
