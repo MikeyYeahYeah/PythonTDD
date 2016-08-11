e8cd51ed66b474854e02d78c422ba98c0cb5fc0f
Michael Rosabal <mrosabal@circleci.com>
2016-08-11 13:26:04 -0400

    Build

--- lists/tests.py
+++ lists/tests.py
@@ -26,4 +26,4 @@ class HomePageTest(TestCase):
       self.assertIn('A new list item', response.content.decode())
       expected_html = render_to_string('home.html',{'new_item_text':  'A new list item'})
       self.assertEqual(response.content.decode(), expected_html)
-      # Test Test
+      # Test Test Test
